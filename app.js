e5cd32bb696da34a1a860bb690598f6bbe4376ea
ywettai-dev <ywettai.dev@gmail.com>
2019-05-25 22:31:32 +0630

    Add weekday.html weekend.html

--- app.js
+++ app.js
@@ -6,10 +6,25 @@ const bodyParser = require('body-parser');
 const port = 3000;
 
 //body-parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 //Express server
 app.listen(process.env.PORT || port, () => console.log(`todolist-v1 starts on ${port}`));
 
 //Home
-app.get('/', (req, res) => res.sendFile(__dirname + `index.html`));
+app.get('/', function (req, res) {
+
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    console.log(__dirname);
+
+    if (currentDay === 0 || currentDay === 6) {
+        res.sendFile(__dirname + `/weekend.html`);
+    } else {
+        res.sendFile(__dirname + `/weekday.html`);
+    }
+
+});
